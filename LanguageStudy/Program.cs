c3615d1e0a10ecdb16716b0278f0e467468a66ab
sgchoo <choosg@naver.com>
2023-10-04 23:47:44 +0900

    Delegate#1

--- LanguageStudy/Program.cs
+++ LanguageStudy/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LanguageStudy
+namespace Delegate
 {
+    delegate int MyDelegate( int a, int b );
+
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Minus(int a, int b)
+        {
+            return (a - b);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Calculator calculator = new Calculator();
+            MyDelegate CallBack;
+
+            CallBack = new MyDelegate(calculator.Plus);
+            Console.WriteLine(CallBack(3, 4));
+
+            CallBack = new MyDelegate(calculator.Minus);
+            Console.WriteLine(CallBack(7, 5));
         }
     }
 }
