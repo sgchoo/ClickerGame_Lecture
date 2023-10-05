a0eb5709c5d671c79ff99ee94913456fc0bb26b2
sgchoo <choosg@naver.com>
2023-10-05 22:44:37 +0900

    Class#1

--- LanguageStudy/Program.cs
+++ LanguageStudy/Program.cs
@@ -5,66 +5,47 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UsingCallback
+namespace Constructor
 {
-    delegate int Compare(int a, int b);
+    class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public Cat() 
+        {
+            Name = "";
+            Color = "";
+        }
+
+        public Cat(string _Name, string _Color)
+        {
+            Name = _Name;
+            Color = _Color;
+        }
+
+        ~Cat()
+        {
+            Console.WriteLine($"{Name} : 잘가");
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : 야옹");
+        }
+    }
 
     class Program
     {
-        static int AscendCompare(int a, int b)
-        {
-            if (a > b)          return 1;
-            else if (a == b)    return 0;
-            else                return -1;
-        }
-
-        static int DescendCompare(int a, int b)
-        {
-            if (a < b)          return 1;
-            else if(a == b)     return 0;
-            else                return -1;
-        }
-
-        static void BubbleSort(int[] Dataset, Compare compare)
-        {
-            int i, j, temp = 0;
-
-            for (i = 0; i < Dataset.Length - 1; i++)
-            {
-                for (j = 0; j < Dataset.Length - (i + 1); j++)
-                {
-                    if (compare(Dataset[j], Dataset[j + 1]) > 0)
-                    {
-                        temp = Dataset[j + 1];
-                        Dataset[j + 1] = Dataset[j];
-                        Dataset[j] = temp;
-                    }
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            int[] array = { 3, 7, 4, 2, 10 };
+            Cat kitty = new Cat("키티", "하얀색");
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
 
-            Console.WriteLine("Sorting ascending...");
-            BubbleSort(array, new Compare(AscendCompare));
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write($"{array[i]} ");
-            }
-
-            int[] array2 = { 7, 2, 8, 10, 11 };
-            Console.WriteLine("\nSorting descending...");
-            BubbleSort(array2, new Compare(DescendCompare));
-
-            for (int i = 0; i < array2.Length; i++)
-            {
-                Console.Write($"{array2[i]} ");
-            }
-
-            Console.WriteLine();
+            Cat nero = new Cat("네로", "검은색");
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} : {nero.Color}");
         }
     }
 }
