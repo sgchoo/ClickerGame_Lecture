43a730afec311d8e09949776e08dc25680ea2f4e
sgchoo <choosg@naver.com>
2023-10-08 00:43:29 +0900

    GenericProgramming#4

--- LanguageStudy/Program.cs
+++ LanguageStudy/Program.cs
@@ -7,78 +7,31 @@ using System.Runtime.Remoting.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConstraintsOnTypeParameters
+namespace UsingGenericList
 {
-    class StructArray<T> where T : struct
-    {
-        public T[] Array { get; set; }
-        public StructArray(int size)
-        {
-            Array = new T[size];
-        }
-    }
-
-    class RefArray<T> where T : class
-    {
-        public T[] Array { get; set; }
-        public RefArray(int size)
-        {
-            Array = new T[size];
-        }
-    }
-
-    class Base { }
-    class Derived : Base { }
-    class BaseArray<U> where U : Base
-    {
-        public U[] Array { get; set; }
-        public BaseArray (int size)
-        {
-            Array = new U[size];
-        }
-
-        public void CopyArray<T>(T[] Source) where T : U
-        {
-            Source.CopyTo(Array, 0);
-        }
-    }
-
     class MainApp
     {
-        public static T CreateInstance<T>() where T : new()
-        {
-            return new T();
-        }
-
         static void Main(string[] arg)
         {
-            StructArray<int> a = new StructArray<int>(3);
-            a.Array[0] = 0;
-            a.Array[1] = 1;
-            a.Array[2] = 2;
+            List<int> list = new List<int>();
+            for(int i = 0; i < 5; i++)
+                list.Add(i);
 
-            RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3);
-            b.Array[0] = new StructArray<double>(5);
-            b.Array[1] = new StructArray<double>(10);
-            b.Array[2] = new StructArray<double>(1005);
+            foreach(int element in list)
+                Console.WriteLine($"{element}");
+            Console.WriteLine();
 
-            BaseArray<Base> c = new BaseArray<Base>(3);
-            c.Array[0] = new Base();
-            c.Array[1] = new Derived();
-            c.Array[2] = CreateInstance<Base>();
+            list.RemoveAt(2);
 
-            BaseArray<Derived> d = new BaseArray<Derived>(3);
-            d.Array[0] = new Derived();
-            d.Array[1] = CreateInstance<Derived>();
-            d.Array[2] = CreateInstance<Derived>();
+            foreach(int element in list)
+                Console.WriteLine($"{element}");
+            Console.WriteLine();
 
-            BaseArray<Derived> e = new BaseArray<Derived>(3);
-            e.CopyArray<Derived>(d.Array);
-
-            for(int i = 0; i < a.Array.Length; i++)
-            {
-                Console.WriteLine(b.Array[i]);
-            }
+            list.Insert(2, 2);
+            
+            foreach (int element in list)
+                Console.WriteLine($"{element}");
+            Console.WriteLine();
         }
     }
 }
